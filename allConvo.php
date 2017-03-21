e0f9cea9cb3493ffab126480f09eb5bc82132a5f
cab13e <cab13e@acu.edu>
2017-03-21 16:08:49 -0500

    adding text for displaying all convos

--- allConvo.php
+++ allConvo.php
@@ -8,3 +8,17 @@ $client = new IntercomClient("dG9rOmFkODRlMjdhX2M2MGVfNDU0Nl85MjRiXzJkY2U5Y2E4Nj
 $response = $client->conversations->getConversations([
     "email" => "johannes.sidarta@revolutionit.com.au"
     ]);
+
+foreach($response->conversations as $conversation)
+{
+   $day = (getdate($conversation->updated_at));
+   // hours min month mday year
+    echo ('<p>' . $day[hours] . ':' . $day[minutes] . ' ' . $day[month] . '/' . $day[mday] . '/' . $day[year] . '</p>');
+    if($conversation->conversation_message->subject != '')
+        echo $conversation->conversation_message->subject;
+    else
+        echo mb_strimwidth( $conversation->conversation_message->body , 0, 20); 
+    // echo $conversation->conversation_message->body;
+    // var_dump($conversation);
+    echo '<hr>';
+}
