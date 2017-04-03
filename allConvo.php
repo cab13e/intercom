e6c8cfa3c27f9389a3a8c292d555342c18fc8606
cab13e <cab13e@acu.edu>
2017-04-03 12:09:22 -0500

    adding code for looking at individual conversations

--- allConvo.php
+++ allConvo.php
@@ -1,6 +1,7 @@
 <?php
 require "vendor/autoload.php";
 use Intercom\IntercomClient;
+session_start();
 
 $client = new IntercomClient("dG9rOmFkODRlMjdhX2M2MGVfNDU0Nl85MjRiXzJkY2U5Y2E4NjY5NToxOjA=", null);
 
@@ -9,16 +10,37 @@ $response = $client->conversations->getConversations([
     "email" => "johannes.sidarta@revolutionit.com.au"
     ]);
 
+
+    ?>
+<html>
+    <head>
+    </head>
+    <body>
+        <script>
+            
+        </script>
+
+<?php
 foreach($response->conversations as $conversation)
 {
+    $id = $conversation->id;
+
    $day = (getdate($conversation->updated_at));
    // hours min month mday year
-    echo ('<p>' . $day[hours] . ':' . $day[minutes] . ' ' . $day[month] . '/' . $day[mday] . '/' . $day[year] . '</p>');
+    echo ("<a href='individualConvo.php?id=" . $id . "'>" . $day[hours] . ':' . $day[minutes] . ' ' . $day[month] . '/' . $day[mday] . '/' . $day[year] . '</a>');
     if($conversation->conversation_message->subject != '')
-        echo $conversation->conversation_message->subject;
+        echo ( $conversation->conversation_message->subject);
     else
         echo mb_strimwidth( $conversation->conversation_message->body , 0, 20); 
+
+    // echo '</br>';
+    // echo $id;
     // echo $conversation->conversation_message->body;
     // var_dump($conversation);
     echo '<hr>';
 }
+?>
+
+
+    </body>
+</html>
--- individualConvo.php
+++ individualConvo.php
@@ -3,7 +3,23 @@ require "vendor/autoload.php";
 use Intercom\IntercomClient;
 
 $client = new IntercomClient("dG9rOmFkODRlMjdhX2M2MGVfNDU0Nl85MjRiXzJkY2U5Y2E4NjY5NToxOjA=", null);
-
+$id = $_GET['id'];
+// echo $id;
 // get individual convo from list
-$response = $client->conversations->getConversation("7270467701");
-var_dump($response);
+$response = $client->conversations->getConversation($id);
+
+// $response = $client->getConversation(array(
+//   "id" => $id
+// ));
+// var_dump($response);
+
+foreach($response->conversation_parts->conversation_parts as $convo)
+   {
+        $day = (getdate($convo->updated_at));
+        echo "At " . $day[hours] . ':' . $day[minutes] . ' ' . $day[month] . '/' . $day[mday] . '/' . $day[year] . " " . $convo->author->type . " said: ";
+       echo $convo->body;
+       echo '<hr>';
+   }
+
+
+// echo $response->conversation_message->body;
